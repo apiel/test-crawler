281fec3d63ec5c66aeeaa9b08da00c3ab592303c
Alexandre Piel <alexandre.piel@gmail.com>
2019-03-02 22:43:58 +0100

    history

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,15 @@
 import { launch } from 'puppeteer';
 import { error, info } from 'npmlog';
-import { writeFile, mkdirSync } from 'fs';
+import { writeFile, mkdirSync, readdirSync } from 'fs';
 import { promisify } from 'util';
 import * as md5 from 'md5';
+import * as rimraf from 'rimraf';
 
 const BASE_URL = 'https://www.zooplus.de/tierarzt';
-const PAGE_FOLDER = `${__dirname}/../pages`;
+const PAGES_FOLDER = `${__dirname}/../pages`;
+const MAX_HISTORY = 2;
 
-async function loadPage(url: string, baseUrl: string, distFolder: string) {
+async function loadPage(url: string, distFolder: string) {
     let hrefs: string[];
     const browser = await launch({
         // headless: false,
@@ -22,7 +24,7 @@ async function loadPage(url: string, baseUrl: string, distFolder: string) {
         const html = await page.content();
         const filename = `${distFolder}/${md5(url)}`;
         await promisify(writeFile)(`${filename}.html`, html);
-        await page.screenshot({path: `${filename}.png`});
+        await page.screenshot({ path: `${filename}.png`, fullPage: true });
         hrefs = await page.$$eval('a', as => as.map(a => (a as any).href));
     } catch (err) {
         error('ERR', err);
@@ -30,15 +32,27 @@ async function loadPage(url: string, baseUrl: string, distFolder: string) {
 
     await browser.close();
 
-    const urls = hrefs.filter(href => href.indexOf(baseUrl) === 0);
+    const urls = hrefs.filter(href => href.indexOf(BASE_URL) === 0);
     console.log('urls', urls);
 }
 
+function cleanHistory() {
+    const folders = readdirSync(PAGES_FOLDER);
+    folders.sort();
+    const cleanUp = folders.slice(0, -(MAX_HISTORY-1));
+    cleanUp.forEach((folder) => {
+        info('Clean up history', folder);
+        rimraf.sync(`${PAGES_FOLDER}/${folder}`);
+    });
+}
+
 function start() {
+    cleanHistory();
     const timestamp = Math.floor(Date.now() / 1000);
-    const distFolder = `${PAGE_FOLDER}/${timestamp}`;
+    const distFolder = `${PAGES_FOLDER}/${timestamp}`;
+    info('Dist folder:', distFolder);
     mkdirSync(distFolder);
-    loadPage(BASE_URL, BASE_URL, distFolder);
+    loadPage(BASE_URL, distFolder);
 }
 
 start();
