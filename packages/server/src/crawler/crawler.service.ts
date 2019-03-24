c55c331ef804567510c0f8de0084fe7b48b5e987
Alexandre Piel <alexandre.piel@gmail.com>
2019-03-24 21:49:02 +0100

    getAll

--- packages/server/src/crawler/crawler.service.ts
+++ packages/server/src/crawler/crawler.service.ts
@@ -2,10 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { CrawlerInput } from './dto/crawler.input';
 import { Crawler } from './models/crawler';
 import { ConfigService } from 'src/config/config.service';
+import { readdir, readJSON } from 'fs-extra';
+import { join } from 'path';
 
 @Injectable()
 export class CrawlerService {
-    constructor(private readonly configService: ConfigService) {}
+    constructor(private readonly configService: ConfigService) { }
 
     async start(crawler: CrawlerInput): Promise<Crawler> {
         console.log('start crawler', crawler);
@@ -13,7 +15,20 @@ export class CrawlerService {
     }
 
     async getAll(): Promise<Crawler[]> {
-        const crawlers: Crawler[] = [];
+        const folders = await readdir(this.configService.config.CRAWL_FOLDER);
+        console.log('folders', folders);
+        const crawlers: Crawler[] = await Promise.all(
+            folders.map(folder => readJSON(join(
+                this.configService.config.CRAWL_FOLDER,
+                folder,
+                '_.json',
+            ))),
+        );
         return crawlers;
     }
 }
+
+// const crawlers: Crawler[] = await Promise.all(
+//     folders.filter(file => extname(file) === '.json')
+//            .map(file => readJSON(file)),
+// );
--- packages/server/src/crawler/models/crawler.ts
+++ packages/server/src/crawler/models/crawler.ts
@@ -2,9 +2,6 @@ import { Field, ID, ObjectType } from 'type-graphql';
 
 @ObjectType()
 export class Crawler {
-  @Field(type => ID)
-  id: string;
-
   @Field()
   url: string;
 }
