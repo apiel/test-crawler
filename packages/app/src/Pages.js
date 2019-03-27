7ae6429689e83f29ce5c10e7cb8a5a830e639cbe
Alexandre Piel <alexandre.piel@gmail.com>
2019-03-27 22:52:10 +0100

    use masonry

--- packages/app/src/Pages.js
+++ packages/app/src/Pages.js
@@ -1,22 +1,17 @@
 import React from 'react';
 import Card from 'antd/lib/card';
 import Spin from 'antd/lib/spin';
-import Row from 'antd/lib/row';
-import Col from 'antd/lib/col';
 import { graphql, withApollo } from 'react-apollo';
-import { withContentRect } from 'react-measure';
+import Masonry from 'react-masonry-component';
 
 import GET_PAGES from './gql/query/getPages';
 
 const cardStyle = {
-    // width: 300,
-    // margin: 5,
+    width: 320,
+    marginBottom: 10,
 };
 
 const coverStyle = {
-    // height: 100,
-    // overflow: 'hidden',
-    // overflowY: 'scroll',
     textAlign: 'center'
 }
 
@@ -24,46 +19,36 @@ const imgStyle = {
     // width: 300,
 }
 
-const rowStyle = {
+const masonryStyle = {
     paddingTop: 10,
     paddingBottom: 10,
 }
 
-const colStyle = {
-    paddingLeft: 10,
+const masonryOptions = {
+    gutter: 10,
+    // percentPosition: true,
+    fitWidth: true,
 }
 
-export const Pages = ({ data: { getPages }, timestamp, measureRef, measure, contentRect: { bounds: { width } } }) => {
-    const colCount = width ? Math.floor(width / 300) : 1;
-    const colMap = Array(colCount).fill(1)
-
-    return getPages ? (
-        <Row
-            style={rowStyle}
-        >
-            <div ref={measureRef}></div>
-            {colMap.map((i, index) => (
-                <Col style={index ? colStyle : {}} span={24/colCount} key={`col-${index}`}>
-                    {getPages.filter((page, fIndex) => index === fIndex%colCount).map(({ id, url, pixelDiffRatio }) => (
-                        <Card
-                            key={id}
-                            hoverable
-                            style={cardStyle}
-                            cover={(
-                                <div style={coverStyle}>
-                                    <img style={imgStyle} alt="" src={`/crawler/thumbnail/${timestamp}/${id}`} />
-                                </div>
-                            )}
-                        >
-                            <p><a href={url}>{url}</a></p>
-                            <p>Pixel diff ratio: {pixelDiffRatio || 0}</p>
-                        </Card>
-                    ))}
-                </Col>
-            ))}
-        </Row>
-    ) : <Spin />;
-}
+export const Pages = ({ data: { getPages }, timestamp }) => getPages ? (
+    <Masonry style={masonryStyle} options={masonryOptions}>
+        {getPages.map(({ id, url, pixelDiffRatio }) => (
+            <Card
+                key={id}
+                hoverable
+                style={cardStyle}
+                cover={(
+                    <div style={coverStyle}>
+                        <img style={imgStyle} alt="" src={`/crawler/thumbnail/${timestamp}/${id}`} />
+                    </div>
+                )}
+            >
+                <p><a href={url}>{url}</a></p>
+                <p>Pixel diff ratio: {pixelDiffRatio || 0}</p>
+            </Card>
+        ))}
+    </Masonry>
+) : <Spin />;
 
 export default graphql(GET_PAGES, {
     options: props => {
@@ -74,4 +59,4 @@ export default graphql(GET_PAGES, {
             },
         };
     },
-})(withApollo(withContentRect('bounds')(Pages)));
+})(withApollo(Pages));
