be8c3c1ab37d0450b493817d76a4f034ab040ce0
Alexandre Piel <alexandre.piel@gmail.com>
2019-04-01 22:36:40 +0200

    review done switch

--- packages/app/src/CrawlerInfo.js
+++ packages/app/src/CrawlerInfo.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import Typography from 'antd/lib/typography';
+import Switch from 'antd/lib/switch';
 
 import { timestampToString } from './utils';
 
 const { Title } = Typography;
 
-export const CrawlerInfo = ({ crawler: { timestamp, url, viewport: { width, height}} }) => (
+export const CrawlerInfo = ({ crawler: { timestamp, url, diffZoneCount, viewport: { width, height}} }) => (
     <>
         <Title level={3}>{timestampToString(timestamp)}</Title>
         <p><b>URL:</b> {url}</p>
         <p><b>Screen:</b> {width}x{height}</p>
+        {diffZoneCount > 0 && <p><Switch checkedChildren="done" unCheckedChildren="review" /></p>}
     </>
 );
