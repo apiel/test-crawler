c343064f663120e1865d8f622687cb7fca3e1219
Alexandre Piel <alexandre.piel@gmail.com>
2019-04-02 20:33:34 +0200

    in progress

--- packages/app/src/CrawlerInfo.js
+++ packages/app/src/CrawlerInfo.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Typography from 'antd/lib/typography';
+import Progress from 'antd/lib/progress';
 
 import { timestampToString } from './utils';
 import SwitchStatus from './SwitchStatus';
@@ -8,13 +9,20 @@ const { Title } = Typography;
 
 export const CrawlerInfo = ({ crawler: {
     timestamp, url, diffZoneCount, status, urlsCount, inQueue, viewport: { width, height } }
-}) => (
+}) => {
+    const total = urlsCount + inQueue;
+    const percent = Math.floor(urlsCount/total*100);
+    return (
         <>
             <Title level={3}>{timestampToString(timestamp)}</Title>
             <p><b>URL:</b> {url}</p>
             <p><b>Screen:</b> {width}x{height}</p>
             <p><b>URL crawled:</b> {urlsCount}</p>
-            {inQueue > 0 && <p><b>In queue:</b> {inQueue}</p>}
+            {inQueue > 0 && <>
+                <Progress percent={percent} />
+                <p><b>In queue:</b> {inQueue}</p>
+            </>}
             {diffZoneCount > 0 && <p><SwitchStatus status={status} timestamp={timestamp} /></p>}
         </>
     );
+}
