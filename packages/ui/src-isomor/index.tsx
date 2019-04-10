900dc3c56d3351c842e390e9afca2def598e76cd
Alexandre Piel <alexandre.piel@gmail.com>
2019-04-10 21:00:40 +0200

    create ui folder

---
+++ packages/ui/src-isomor/index.tsx
@@ -0,0 +1,13 @@
+import { IsomorProvider } from 'isomor-react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+
+ReactDOM.render((<IsomorProvider><App /></IsomorProvider>), document.getElementById('root'));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
