82218fcab0b580b702eb69c3f2db1ff52daa7ad5
Alexandre Piel <alexandre.piel@gmail.com>
2019-04-16 19:31:46 +0200

    add duration

--- packages/test-crawler-ui/src/CrawlerInfo.tsx
+++ packages/test-crawler-ui/src/CrawlerInfo.tsx
@@ -1,23 +1,38 @@
 import React from 'react';
 import Typography from 'antd/lib/typography';
 import Progress from 'antd/lib/progress';
+import { Crawler } from 'test-crawler-lib';
+import { duration } from 'moment';
+import 'moment-duration-format';
 
 import { timestampToString } from './utils';
 import { SwitchStatus } from './SwitchStatus';
 
 const { Title } = Typography;
 
-export const CrawlerInfo = ({ crawler: {
-    timestamp, url, diffZoneCount, status, urlsCount, inQueue, viewport: { width, height } }
-}: any) => {
+// need to flatten props and use react memo
+export const CrawlerInfo = ({
+    crawler: {
+        timestamp,
+        url,
+        diffZoneCount,
+        status,
+        urlsCount,
+        inQueue,
+        startAt,
+        lastUpdate,
+        viewport: { width, height },
+    }
+}: { crawler: Crawler }) => {
     const total = urlsCount + inQueue;
-    const percent = Math.floor(urlsCount/total*100);
+    const percent = Math.floor(urlsCount / total * 100);
     return (
         <>
             <Title level={3}>{timestampToString(timestamp)}</Title>
             <p><b>URL:</b> {url}</p>
             <p><b>Screen:</b> {width}x{height}</p>
             <p><b>URL crawled:</b> {urlsCount}</p>
+            <p><b>Duration:</b> { duration(lastUpdate - startAt).format('h [hrs], m [min], s [sec]') }</p>
             {inQueue > 0 && <>
                 <Progress percent={percent} />
                 <p><b>In queue:</b> {inQueue}</p>
