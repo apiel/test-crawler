f4cd15a7e9c22b78031e25ad7cc342b650aff2d1
Alexandre Piel <alexandre.piel@gmail.com>
2019-04-18 21:33:56 +0200

    mutex

--- packages/test-crawler-lib/dist/bin/index.js
+++ packages/test-crawler-lib/dist/bin/index.js
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const lockfile_1 = require("lockfile");
+const npmlog_1 = require("npmlog");
 const crawl_1 = require("./crawl");
-crawl_1.crawl();
+const path_1 = require("path");
+const lockFile = path_1.join(__dirname, '../../test-crawler.lock');
+if (!lockfile_1.checkSync(lockFile)) {
+    lockfile_1.lockSync(lockFile);
+    crawl_1.crawl();
+}
+else {
+    npmlog_1.info('Test-crawler already running', lockFile);
+}
 //# sourceMappingURL=index.js.map
--- packages/test-crawler-lib/src/bin/index.ts
+++ packages/test-crawler-lib/src/bin/index.ts
@@ -1,4 +1,16 @@
 #!/usr/bin/env node
 
+import { checkSync, lockSync } from 'lockfile';
+import { info } from 'npmlog';
+
 import { crawl } from './crawl';
-crawl();
+import { join } from 'path';
+
+const lockFile = join(__dirname, '../../test-crawler.lock');
+
+if (!checkSync(lockFile)) {
+    lockSync(lockFile);
+    crawl();
+} else {
+    info('Test-crawler already running', lockFile);
+}
