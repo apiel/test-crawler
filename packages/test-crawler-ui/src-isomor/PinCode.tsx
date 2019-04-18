3f9032ffef83998ad53de85813d0c806f42242b8
Alexandre Piel <alexandre.piel@gmail.com>
2019-04-18 16:12:23 +0200

    ace editor

--- packages/test-crawler-ui/src-isomor/PinCode.tsx
+++ packages/test-crawler-ui/src-isomor/PinCode.tsx
@@ -3,6 +3,10 @@ import Spin from 'antd/lib/spin';
 import Typography from 'antd/lib/typography';
 import Card from 'antd/lib/card';
 import Icon from 'antd/lib/icon';
+import AceEditor from 'react-ace';
+
+import 'brace/mode/javascript';
+import 'brace/theme/tomorrow';
 
 import {
     cardStyle,
@@ -14,6 +18,18 @@ import { getPin } from './server/crawler';
 import { RouteComponentProps } from 'react-router';
 
 const { Title } = Typography;
+
+const aceEditorStyle = {
+    border: '1px solid #EEE',
+    marginBottom: 15,
+    marginRight: 15,
+    float: 'left' as 'left',
+}
+
+const cardRightStyle = {
+    ...cardStyle,
+    float: 'left' as 'left',
+}
 
 export const PinCode = ({ match: { params: { id } } }: RouteComponentProps<any>) => {
     const [pin, setPin] = React.useState<PageData>();
@@ -28,8 +44,17 @@ export const PinCode = ({ match: { params: { id } } }: RouteComponentProps<any>)
             {
                 pin ? (
                         <>
+                            <AceEditor
+                                mode="javascript"
+                                theme="tomorrow"
+                                // onLoad={this.onLoad}
+                                // onChange={this.onChange}
+                                fontSize={14}
+                                value={``}
+                                style={aceEditorStyle}
+                            />
                             <Card
-                                style={cardStyle}
+                                style={cardRightStyle}
                                 cover={pin.png && <DiffImage folder='base' id={pin.id} />}
                             >
                                 <p><Icon type="link" /> <a href={pin.url}>{pin.url}</a></p>
