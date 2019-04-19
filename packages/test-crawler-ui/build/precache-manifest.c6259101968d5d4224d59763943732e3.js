11f026e4ddf5def35ce084d83042b999cffea839
Alexandre Piel <alexandre.piel@gmail.com>
2019-04-19 18:40:51 +0200

    dont ignore build

---
+++ packages/test-crawler-ui/build/precache-manifest.c6259101968d5d4224d59763943732e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9a751574b5f100779b1f",
+    "url": "/static/css/main.b773a374.chunk.css"
+  },
+  {
+    "revision": "9a751574b5f100779b1f",
+    "url": "/static/js/main.6ef6641e.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "61f931bb0449474a2a30",
+    "url": "/static/css/2.706451a8.chunk.css"
+  },
+  {
+    "revision": "61f931bb0449474a2a30",
+    "url": "/static/js/2.5791d9f0.chunk.js"
+  },
+  {
+    "revision": "fdbe75d86875129cf0f4edbe66686358",
+    "url": "/index.html"
+  }
+];
