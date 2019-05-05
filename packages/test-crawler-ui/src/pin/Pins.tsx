da96bb6e2d26a8b60f9903296c72c8a65c5e921b
Alexandre Piel <alexandre.piel@gmail.com>
2019-05-05 08:20:16 +0200

    use generic filter in pins

--- packages/test-crawler-ui/src/pin/Pins.tsx
+++ packages/test-crawler-ui/src/pin/Pins.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Spin from 'antd/lib/spin';
 import Typography from 'antd/lib/typography';
-import Input from 'antd/lib/input';
 import Masonry from 'react-masonry-component';
 
 import {
@@ -12,22 +11,16 @@ import { getPins } from '../server/crawler';
 import { PageData } from 'test-crawler-lib';
 import { useAsyncCacheEffect } from 'react-async-cache';
 import { ErrorHandler } from '../common/ErrorHandler';
-import { onSearch, searchStyle } from '../search/search';
 import { PinPage } from './PinPage';
+import { Search } from '../search/Search';
 
 const { Title } = Typography;
-const { Search } = Input;
 
 export const Pins = () => {
     const { response, error } = useAsyncCacheEffect<PageData[]>(getPins);
     if (error) {
         return <ErrorHandler description={error.toString()} />;
     }
-
-    const [pins, setPins] = React.useState<PageData[]>();
-    React.useEffect(() => {
-        setPins(response);
-    }, [response]);
 
     let masonry: any;
     let timer: NodeJS.Timer;
@@ -41,31 +34,27 @@ export const Pins = () => {
     return (
         <>
             <Title level={3}>Pins</Title>
-            <Search
-                placeholder="Search"
-                onChange={onSearch(setPins, response)}
-                style={searchStyle}
-                allowClear
-            />
-            {pins ? (
-                <Masonry
-                    style={masonryStyle}
-                    options={masonryOptions}
-                    ref={(c: any) => { masonry = c && c.masonry; }}
-                >
-                    {pins.map(({ id, url, png, viewport }: PageData) => (
-                        <PinPage
-                            id={id}
-                            key={id}
-                            url={url}
-                            png={png}
-                            viewport={viewport}
-                            onImg={onImg}
-                        />
-                    ))}
-                </Masonry>
-            ) : <Spin />
-            }
+            <Search response={response}>
+                {(pins) => pins ? (
+                    <Masonry
+                        style={masonryStyle}
+                        options={masonryOptions}
+                        ref={(c: any) => { masonry = c && c.masonry; }}
+                    >
+                        {pins.map(({ id, url, png, viewport }: PageData) => (
+                            <PinPage
+                                id={id}
+                                key={id}
+                                url={url}
+                                png={png}
+                                viewport={viewport}
+                                onImg={onImg}
+                            />
+                        ))}
+                    </Masonry>
+                ) : <Spin />
+                }
+            </Search>
         </>
     );
 }
