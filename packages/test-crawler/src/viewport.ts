4f6435b559624da3d0bc2cb4baa3b9335c7e14c7
Alexandre Piel <alexandre.piel@gmail.com>
2020-01-23 15:25:38 +0100

    update viewport

--- packages/test-crawler/src/viewport.ts
+++ packages/test-crawler/src/viewport.ts
@@ -13,7 +13,6 @@ export interface Viewport {
 export const viewports: Viewport[] = [
     { name: 'Desktop - 800x600', value: { width: 800, height: 600 } },
     { name: 'Desktop - 1024x768', value: { width: 1024, height: 768 } },
-    { name: 'Desktop - 1920x1080', value: { width: 1920, height: 1080 } },
     { name: 'iPhone 5 - 320x568', value: { width: 320, height: 568, isMobile: true, hasTouch: true } },
     { name: 'iPhone 6 - 375x667', value: { width: 375, height: 667, isMobile: true, hasTouch: true } },
     { name: 'Ipad - 768x1024', value: { width: 768, height: 1024, isMobile: true, hasTouch: true } },
