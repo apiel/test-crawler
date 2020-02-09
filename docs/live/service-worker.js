b1515d99700f53fe63de4314c6e9e9171cce8da2
Alexandre Piel <alexandre.piel@gmail.com>
2020-02-09 18:06:06 +0100

    yo

--- docs/live/service-worker.js
+++ docs/live/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.10927cbe2daeaa4f7181bf9c7ecf5a0e.js"
+  "/test-crawler/live/precache-manifest.cca75293f4aba1d30f2208c0b89f719c.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/test-crawler/live/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
