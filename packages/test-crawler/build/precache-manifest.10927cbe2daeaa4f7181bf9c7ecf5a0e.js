80bd817e1652aed909607225c2fc320473af3911
Alexandre Piel <alexandre.piel@gmail.com>
2020-02-09 17:40:49 +0100

    pub

---
+++ packages/test-crawler/build/precache-manifest.10927cbe2daeaa4f7181bf9c7ecf5a0e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "879b072dd26f3e04a5e9b9cdde13e927",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2323aa1e3b513e2679da",
+    "url": "/static/css/2.0b61d4ae.chunk.css"
+  },
+  {
+    "revision": "8c3797e235f482fd8f8f",
+    "url": "/static/css/main.56451939.chunk.css"
+  },
+  {
+    "revision": "2323aa1e3b513e2679da",
+    "url": "/static/js/2.98aac632.chunk.js"
+  },
+  {
+    "revision": "9b2cf3ab1f2ff5b5fa886b76aad7a8de",
+    "url": "/static/js/2.98aac632.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c3797e235f482fd8f8f",
+    "url": "/static/js/main.76daa89b.chunk.js"
+  },
+  {
+    "revision": "33c2392257a17714c1b6",
+    "url": "/static/js/runtime-main.b0f1abea.js"
+  }
+]);
