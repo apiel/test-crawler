9bdce3c2bd9d10f689c8402decf7670f5203d94c
Alexandre Piel <alexandre.piel@gmail.com>
2020-02-16 22:04:56 +0100

    applychanges

--- packages/test-crawler/src/pages/PagesActionZone.tsx
+++ packages/test-crawler/src/pages/PagesActionZone.tsx
@@ -5,14 +5,23 @@ import notification from 'antd/lib/notification';
 import { setZonesStatus } from '../server/service';
 import { PageData } from '../server/typing';
 import { StorageType } from '../server/storage.typing';
+import { useApplyChanges, ChangeItem } from '../hook/useApplyChanges';
 
 const onClick = (
+    add: (changeItem: ChangeItem) => void,
     { timestamp, id, status, projectId, pages, setPages, storageType }: Props,
 ) => () => {
     // try {
     // const pages = await setZonesStatus(storageType, projectId, timestamp.toString(), id, status);
     const pageIndex = pages.findIndex(({ id: pageId }) => id === pageId);
-    pages[pageIndex].png?.diff?.zones?.forEach(zone => zone.status = status);
+    pages[pageIndex].png?.diff?.zones?.forEach((zone, index) => {
+        add({
+            key: ['setZoneStatus', storageType, projectId, timestamp, id, index].join('-'),
+            type: 'setZoneStatus',
+            args: [storageType, projectId, timestamp, id, index, status],
+        });
+        zone.status = status;
+    });
     setPages([...pages]); // make a copy else changes don't apply
     //     message.success(`All zone are set to "${status}".`, 2);
     // } catch (error) {
@@ -34,8 +43,9 @@ interface Props {
     type: string;
 }
 export const PagesActionZone = (props: Props) => {
+    const { add } = useApplyChanges();
     const { status, type } = props;
     return (
-        <Icon type={type} title={`Set all zone to "${status}".`} onClick={onClick(props)} />
+        <Icon type={type} title={`Set all zone to "${status}".`} onClick={onClick(add, props)} />
     );
 }
