0db9d8d4957fb9fa8d69734f558843322a860c7d
Alexandre Piel <alexandre.piel@gmail.com>
2020-02-22 12:55:24 +0100

    build

---
+++ packages/test-crawler-cli/src/cli/cli.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const logol_1 = require("logol");
+const lib_1 = require("../lib");
+const configs = require("../lib/config");
+function start() {
+    return __awaiter(this, void 0, void 0, function* () {
+        logol_1.info('Test-crawler');
+        logol_1.info('Config', configs);
+        const [, , option, value] = process.argv;
+        if (option && value) {
+            if (option === '--project') {
+                const crawlTarget = {
+                    projectId: value,
+                    timestamp: lib_1.generateTinestampFolder(),
+                };
+                logol_1.info('Start project', crawlTarget);
+                const result = yield lib_1.crawl(crawlTarget, 30);
+                logol_1.info('result', result);
+                return;
+            }
+        }
+        lib_1.crawl(undefined, 30);
+    });
+}
+start();
+//# sourceMappingURL=cli.js.map
