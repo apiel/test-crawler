eebef7259ddbd8fbf255e16763a4d4ee05507455
Alexandre Piel <alexandre.piel@gmail.com>
2020-02-22 23:49:58 +0100

    try to fix crawl action

--- actions/crawl/index.js
+++ actions/crawl/index.js
@@ -1,12 +1,17 @@
 const core = require('@actions/core');
 const { crawl } = require('test-crawler-cli');
+const { generateTinestampFolder } = require('test-crawler-core');
 
 async function run() {
     try {
         core.info('Run test-crawler');
         const projectId = core.getInput('projectId');
 
-        crawl(projectId, 30);
+        const crawlTarget = {
+            projectId,
+            timestamp: generateTinestampFolder(),
+        };
+        crawl(crawlTarget, 30);
     } catch (error) {
         core.setFailed(error.message);
     }
