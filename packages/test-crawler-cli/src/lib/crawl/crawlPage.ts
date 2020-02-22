86db799285e25f8098fc64e6ee273fca55faff87
Alexandre Piel <alexandre.piel@gmail.com>
2020-02-22 19:39:37 +0100

    pub

--- packages/test-crawler-cli/src/lib/crawl/crawlPage.ts
+++ packages/test-crawler-cli/src/lib/crawl/crawlPage.ts
@@ -1,5 +1,5 @@
-// import { Page } from 'puppeteer';
-type Page = any;
+import { Page } from 'puppeteer';
+// type Page = any;
 import { error, info, warn } from 'logol';
 import { readJSON, pathExists, outputJson } from 'fs-extra';
 import * as minimatch from 'minimatch';
