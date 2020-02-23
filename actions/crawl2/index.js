2116ed3fb51d6bf01b4ddec0d8d39da18648f3fb
Alexandre Piel <alexandre.piel@gmail.com>
2020-02-23 13:44:00 +0100

    fix path crawl2

--- actions/crawl2/index.js
+++ actions/crawl2/index.js
@@ -1,6 +1,7 @@
 const core = require('@actions/core');
 const exec = require('@actions/exec');
 
+const { join } = require('path');
 const { crawl } = require('test-crawler-cli');
 const { generateTinestampFolder } = require('test-crawler-core');
 
@@ -15,7 +16,7 @@ async function run() {
         await exec.exec(`yarn add geckodriver`);
 
         core.info(`Run for project ${projectId}`);
-        await exec.exec(`node crawl.js ${projectId}`);
+        await exec.exec(`node ${join(__dirname, 'crawl.js')} ${projectId}`);
     } catch (error) {
         core.setFailed(error.message);
     }
