a747af281fe3b8934caa9dcc2add5711c5f9385b
Alexandre Piel <alexandre.piel@gmail.com>
2020-02-24 18:29:10 +0100

    cli

--- packages/test-crawler-driver-manager/src/cli/cli.ts
+++ packages/test-crawler-driver-manager/src/cli/cli.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
-import { info } from 'logol';
+import { info, warn } from 'logol';
+import * as os from 'os';
 
 import {
     getGeckodriver,
@@ -8,15 +9,56 @@ import {
     getChromedriver,
     Arch,
     getIedriver,
+    Options,
 } from '../lib';
+
+enum Driver {
+    Gecko = 'Gecko',
+    Chrome = 'Chrome',
+    IE = 'IE',
+}
+
+interface Item {
+    driver: Driver;
+    options?: Options;
+}
 
 async function start() {
     info('Test-crawler driver manager');
 
-    await getGeckodriver({ platform: Platform.win });
-    await getGeckodriver({ platform: Platform.mac });
-    await getChromedriver({ platform: Platform.win });
-    await getIedriver({ platform: Platform.win, arch: Arch.x32 });
+    const [, , jsonItems, destination] = process.argv;
+
+    if (!jsonItems) {
+        warn('Missing parameter, please provide json.');
+        info(
+            `test-crawler-driver-manager '${JSON.stringify([
+                { driver: 'Chrome', options: { platform: 'darwin' } },
+            ])}' optional_destination_folder`,
+        );
+        info('Driver', Driver);
+        info('Platform', Platform);
+        info('Arch', Arch);
+        return;
+    }
+
+    const items: Item[] = JSON.parse(jsonItems);
+    for (const item of items) {
+        const option = {
+            platform: os.platform(),
+            arch: os.arch() as Arch,
+            destination,
+            ...item.options,
+        }
+        if (item.driver === Driver.Chrome) {
+            await getChromedriver(option);
+        } else if (item.driver === Driver.Gecko) {
+            await getGeckodriver(option);
+        } else if (item.driver === Driver.IE) {
+            await getIedriver(option);
+        } else {
+            warn('Unknown driver', item.driver);
+        }
+    }
 }
 
 start();
